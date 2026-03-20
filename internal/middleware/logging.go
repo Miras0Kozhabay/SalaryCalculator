5c8ee62a32bdc378ca15938589faecd162421770
Miras0Kozhabay <91242318+Miras0Kozhabay@users.noreply.github.com>
2026-03-21 03:45:05 +0500

    feat: add HTTP logging middleware

--- internal/middleware/logging.go
+++ internal/middleware/logging.go
@@ -7,18 +7,25 @@ import (
 )
 
 func Logging(next http.Handler) http.Handler {
-
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
 		start := time.Now()
 
-		next.ServeHTTP(w, r)
+		// Wrap ResponseWriter to capture status
+		lrw := &loggingResponseWriter{w, http.StatusOK}
+		next.ServeHTTP(lrw, r)
 
-		log.Printf(
-			"%s %s %s",
-			r.Method,
-			r.URL.Path,
-			time.Since(start),
-		)
+		duration := time.Since(start)
+
+		log.Printf("%s %s %d %s", r.Method, r.URL.Path, lrw.statusCode, duration)
 	})
+}
+
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (lrw *loggingResponseWriter) WriteHeader(code int) {
+	lrw.statusCode = code
+	lrw.ResponseWriter.WriteHeader(code)
 }
