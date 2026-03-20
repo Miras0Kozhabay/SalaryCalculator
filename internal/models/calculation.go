18ab81068080d21f6d839054e77c1778fb062df6
Miras0Kozhabay <91242318+Miras0Kozhabay@users.noreply.github.com>
2026-03-21 03:26:50 +0500

    feat: add calculation models

---
+++ internal/models/calculation.go
@@ -0,0 +1,40 @@
+package models
+
+import "time"
+
+type Calculation struct {
+	ID int64 `json:"id"`
+
+	GrossSalary float64 `json:"gross_salary"`
+	NetSalary   float64 `json:"net_salary"`
+
+	OPV   float64 `json:"opv"`
+	IPN   float64 `json:"ipn"`
+	VOSMS float64 `json:"vosms"`
+
+	SO    float64 `json:"so"`
+	SN    float64 `json:"sn"`
+	OOSMS float64 `json:"oosms"`
+
+	Mode string `json:"mode"`
+
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type CalculateRequest struct {
+	Amount float64 `json:"amount"`
+	Mode   string  `json:"mode"`
+}
+
+type CalculateResponse struct {
+	GrossSalary float64 `json:"gross_salary"`
+	NetSalary   float64 `json:"net_salary"`
+
+	OPV   float64 `json:"opv"`
+	IPN   float64 `json:"ipn"`
+	VOSMS float64 `json:"vosms"`
+
+	SO    float64 `json:"so"`
+	SN    float64 `json:"sn"`
+	OOSMS float64 `json:"oosms"`
+}
