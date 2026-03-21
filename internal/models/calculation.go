ba63d0cf313c447b893f16937f919ba9f093e917
Miras0Kozhabay <91242318+Miras0Kozhabay@users.noreply.github.com>
2026-03-21 19:15:25 +0500

    feat: add EmployerTotal field to Calculation model

--- internal/models/calculation.go
+++ internal/models/calculation.go
@@ -3,22 +3,18 @@ package models
 import "time"
 
 type Calculation struct {
-	ID int64 `json:"id"`
-
-	GrossSalary float64 `json:"gross_salary"`
-	NetSalary   float64 `json:"net_salary"`
-
-	OPV   float64 `json:"opv"`
-	IPN   float64 `json:"ipn"`
-	VOSMS float64 `json:"vosms"`
-
-	SO    float64 `json:"so"`
-	SN    float64 `json:"sn"`
-	OOSMS float64 `json:"oosms"`
-
-	Mode string `json:"mode"`
-
-	CreatedAt time.Time `json:"created_at"`
+	ID            int64     `json:"id"`
+	GrossSalary   float64   `json:"gross_salary"`
+	NetSalary     float64   `json:"net_salary"`
+	OPV           float64   `json:"opv"`
+	IPN           float64   `json:"ipn"`
+	VOSMS         float64   `json:"vosms"`
+	SO            float64   `json:"so"`
+	SN            float64   `json:"sn"`
+	OOSMS         float64   `json:"oosms"`
+	EmployerTotal float64   `json:"employer_total"`
+	Mode          string    `json:"mode"`
+	CreatedAt     time.Time `json:"created_at"`
 }
 
 type CalculateRequest struct {
